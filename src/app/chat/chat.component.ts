21509db36bd832b2439720e88de6293934b53b23
pazi09 <youngsaintforwait@gmail.com>
2022-02-13 19:35:00 +0400

    Chat component update

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -3,6 +3,8 @@ import {WebSocketService} from "../service/web-socket.service";
 import {NgForm} from "@angular/forms";
 import {ChatMessageDto} from "../models/chatMessageDto";
 import {TokenStorageService} from "../_services/token-storage.service";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {AuthInterceptor} from "../_/auth.interceptor";
 
 @Component({
   selector: 'app-chat',
@@ -12,6 +14,7 @@ import {TokenStorageService} from "../_services/token-storage.service";
 export class ChatComponent implements OnInit, OnDestroy {
   currentUser: any;
   isLoggedIn=false;
+  username:any;
 
   constructor(public webSocketService: WebSocketService,
               private token: TokenStorageService,
--- src/app/models/chatMessageDto.ts
+++ src/app/models/chatMessageDto.ts
@@ -2,6 +2,7 @@ export class ChatMessageDto {
   message: string;
 
   constructor(message: string){
+
     this.message = message;
   }
 }
